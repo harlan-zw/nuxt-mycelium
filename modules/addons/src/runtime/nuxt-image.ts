5788b2b1c53d44a1a07c2fbfb26f3fc8cf3f2259
Harlan Wilton <harlan@harlanzw.com>
2022-08-07 23:09:13 +1000

    fix(addons): nuxt-image logic

--- modules/addons/src/runtime/nuxt-image.ts
+++ modules/addons/src/runtime/nuxt-image.ts
@@ -1,6 +1,39 @@
 import { visit } from 'unist-util-visit'
 import type { ContentTransformer, MarkdownNode } from '@nuxt/content/dist/runtime/types'
-import { computeSizes } from '../logic'
+
+export const MaxWidth = 900
+export const MaxHeight = 700
+
+export const computeSizes = (width: number, height: number, maxHeight?: number) => {
+  maxHeight = maxHeight || (height > MaxHeight ? MaxHeight : height)
+  // figure out aspect ratio
+  const ar = width / height
+  // 0.625
+
+  if (width > MaxWidth) {
+    width = MaxWidth
+    height = Math.round(width / ar)
+  }
+
+  if (height > maxHeight) {
+    height = maxHeight
+    width = Math.round(height * ar)
+  }
+
+  const px = (size: number) => `${size}px`
+
+  const sizes = [
+    `md:${px(width)}`,
+    'sm:95vw',
+    'xs:95vw',
+  ].join(' ')
+
+  return {
+    height,
+    width,
+    sizes,
+  }
+}
 
 export default <ContentTransformer> {
   name: 'nuxt-image',
